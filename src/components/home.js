0f97138dcccbf28c9b9c4b81f7aa3bd0c4d2a944
emilpriver <hello@emilpriver.com>
2019-01-21 23:40:54 +0100

    första

---
+++ src/components/home.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+
+
+export default class Home extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+          images: [],
+          images: false,
+        }    
+      }
+
+  render() {
+    return (
+        <div>
+      
+        </div>
+    );
+  }
+}
+
+
